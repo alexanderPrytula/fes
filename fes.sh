7d4cb09b5db838d76dda116c945da913bf07ce4a
alexanderPrytula <isitinme88@gmail.com>
2017-02-02 18:13:43 +0200

    gitmodules check started

--- fes.sh
+++ fes.sh
@@ -1,18 +1,56 @@
 #!/bin/bash
 
-# Flow and ESLint checker for modified files
+# eslint and flow check for modified files
 
-if [[ -z `which flow` || -z `which eslint` ]]; then
-	echo "No required bin (flow, eslint) was found in the \$PATH"
+# important! call only from application root directory, e.g:
+# $ pwd
+# > path/to/my/App
+# $ ./sh/fes
+# ...launching
+
+if [[ -z `which flow` ]]; then
+	echo "No 'flow' binary found. Try: npm i -g flow"
+	exit 1
+elif [[ -z `which eslint` ]]; then
+	echo "No 'eslint' binary found. Try: npm i -g eslint"
 	exit 1
 fi
 
+gitmodules=`pwd`"/.gitmodules"
+
+if [[ -f "$gitmodules" ]]; then
+	declare -a paths=($(grep path "$gitmodules" | awk '{print $3}'))
+
+	concat_path=""
+	for path in "${paths[@]}"; do
+		concat_path+=$path
+	done
+fi
+
 git status --short
-declare -a files=($(git status --short | egrep '\bM\b' | awk '{print $2}'))
+declare -a files=($(git status --short | grep M | grep -v "$concat_path" | awk '{print $2}')) 
+
+echo "${paths[@]}"
+echo "${files[@]}"
 
 for file in "${files[@]}"; do
-	echo "===== Flow check for =====: $file"
-	flow $file
-	echo "===== Eslint check for =====: $file"
-	eslint $file
+	for path in "${paths[@]}"; do
+		# figure out ..
+		if [[ $file =~ $path ]]; then
+			continue
+		else
+			modules_match="true"
+			break
+		fi
+	done
+
+	if [[ -z $modules_match ]]; then
+		# echo "========== Flow check for: $file ==========="
+		# flow $file
+		# echo "========== Eslint check for: $file ==========="
+		# eslint $file
+		echo "$file is not matches gitmodules paths"
+	else
+		echo "$file do matches gitmodules paths"
+	fi
 done
